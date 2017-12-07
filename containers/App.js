6752021eb1618a5b901a75d4a8ecee6f4e58322b
Hannah Swift <h.graceswift@gmail.com>
2017-12-07 16:45:01 -0600

    Convert to simple spa

--- containers/App.js
+++ containers/App.js
@@ -2,6 +2,18 @@ import React, {Component} from 'react';
 
 export default class App extends Component {
     render () {
-        return <p>Testing 1, 2, 3</p>
+        return (
+          <div>
+            <h1>New SPA</h1>
+            <ul className="header">
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+            <div className="content">
+
+            </div>
+          </div>
+        );
     }
 }
