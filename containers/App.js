1fe209952fb0f769b4f416aa9fe2d075f5c080a9
Hannah Swift <h.graceswift@gmail.com>
2017-12-07 16:58:57 -0600

    Add simple spa components

--- containers/App.js
+++ containers/App.js
@@ -1,19 +1,31 @@
 import React, {Component} from 'react';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 export default class App extends Component {
     render () {
         return (
+          <HashRouter>
           <div>
             <h1>New SPA</h1>
             <ul className="header">
-              <li><a href="/">Home</a></li>
-              <li><a href="/about">About</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
             </ul>
             <div className="content">
-
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
             </div>
           </div>
+          </HashRouter>
         );
     }
 }
---
+++ containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <p>Latin placeholder text here.</p>
+
+        <p>Et tu Brute?</p>
+      </div>
+    );
+  }
+}
+
+export default Home
