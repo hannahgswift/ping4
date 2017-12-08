9b17551cfb44195460bb6dc2accd341ec6fc434a
Hannah Swift <h.graceswift@gmail.com>
2017-12-07 18:13:59 -0600

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import Home from "./Home";
 import About from "./About";
 import Contact from "./Contact";
+import "./index.scss";
 
 class App extends React.Component {
     render () {
@@ -15,7 +16,7 @@ class App extends React.Component {
           <div>
             <h1>New SPA</h1>
             <ul className="header">
-              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink exact to="/">Home</NavLink></li>
               <li><NavLink to="/about">About</NavLink></li>
             <li><NavLink to="/contact">Contact</NavLink></li>
             </ul>
